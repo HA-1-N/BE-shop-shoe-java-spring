38aeb99143b06bca4e0fe48b35a69f923cf285df
HA-1-N <hoanganh02062001@gmail.com>
2023-11-03 17:07:35 +0700

    fix(security): role

--- src/main/java/com/example/shopshoejavaspring/repository/UserRepository.java
+++ src/main/java/com/example/shopshoejavaspring/repository/UserRepository.java
@@ -19,7 +19,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Page<User> findAllByNameContainsAndEmailContains(@Param("name") String name, @Param("email") String email , Pageable pageable);
 
-    @Query(value = "SELECT u.*, r.id, r.code, r.text FROM user u inner join user_role ur on ur.user_id = u.id left join role r on r.id = ur.role_id WHERE u.email LIKE %:email%", nativeQuery = true)
+    @Query(value = "SELECT u FROM User u join fetch u.roles WHERE u.email LIKE %:email%")
     Optional<User> findByEmail(@Param("email") String email);
 
     @Query(value = "SELECT r.id, r.code, r.text FROM role r INNER JOIN user_role ur ON r.id = ur.role_id WHERE ur.user_id = :id", nativeQuery = true)
