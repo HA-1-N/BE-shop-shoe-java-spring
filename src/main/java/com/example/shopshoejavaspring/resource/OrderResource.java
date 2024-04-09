888829c5ce0a69289c45c3558698fc6c3c0820a6
DESKTOP-ESNC61C\FPT <anhnh133@fpt.com>
2024-04-09 15:39:52 +0700

    feat(order): build api get by id

--- src/main/java/com/example/shopshoejavaspring/resource/OrderResource.java
+++ src/main/java/com/example/shopshoejavaspring/resource/OrderResource.java
@@ -1,12 +1,15 @@
 package com.example.shopshoejavaspring.resource;
 
 import com.example.shopshoejavaspring.dto.order.OrderCheckoutDTO;
+import com.example.shopshoejavaspring.dto.order.OrderDTO;
 import com.example.shopshoejavaspring.entity.Order;
 import com.example.shopshoejavaspring.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/order")
@@ -17,16 +20,23 @@ public class OrderResource {
     private final OrderService orderService;
 
     @PostMapping("/checkout")
-    public ResponseEntity<OrderCheckoutDTO> checkout (@RequestBody OrderCheckoutDTO orderCheckoutDTO) {
+    public ResponseEntity<OrderCheckoutDTO> checkout(@RequestBody OrderCheckoutDTO orderCheckoutDTO) {
         log.info("OrderCheckoutDTO: {}", orderCheckoutDTO);
         orderService.checkout(orderCheckoutDTO);
         return ResponseEntity.status(201).body(orderCheckoutDTO);
     }
 
     @GetMapping("/get-order-by-user-id/{userId}")
-    public ResponseEntity<Order> getOrderByUserId(@PathVariable Long userId) {
+    public ResponseEntity<List<OrderDTO>> getOrderByUserId(@PathVariable Long userId) {
         log.info("REST request to get order by user id : {}", userId);
-        Order order = orderService.getOrderByUserId(userId);
+        List<OrderDTO> orders = orderService.getOrderByUserId(userId);
+        return ResponseEntity.ok().body(orders);
+    }
+
+    @GetMapping("/get-order-by-id/{orderId}")
+    public ResponseEntity<OrderDTO> getOrderById(@PathVariable Long orderId) {
+        log.info("REST request to get order by id : {}", orderId);
+        OrderDTO order = orderService.getOrderById(orderId);
         return ResponseEntity.ok().body(order);
     }
 
